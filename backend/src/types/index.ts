424c5369d6c320706a70789ebd06f0d16f15c1df
Ramanchoudhary07 <ramantarar1@gmail.com>
2025-12-08 14:25:50 +0530

    (server) : Backend folder structure created

---
+++ backend/src/types/index.ts
@@ -0,0 +1,43 @@
+export type LeadStatus = "New" | "Contacted" | "Converted" | "Lost";
+
+export interface LeadType {
+  _id?: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: number;
+  status: LeadStatus;
+  assignedTo?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface AgentType {
+  _id?: string;
+  name: string;
+  email: string;
+  phone: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface FollowUpType {
+  _id?: string;
+  leadId: string;
+  agentId: string;
+  notes: string;
+  followUpDate: Date;
+  isCompleted: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export interface AnalyticsType {
+  agentId: string;
+  totalLeads: number;
+  convertedLeads: number;
+  conversionPercentage: number;
+  lostLeads: number;
+  contactedLeads: number;
+  newLeads: number;
+}
